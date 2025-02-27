132ee6a62295edba1a39b222fe3c641e45ab02e7
getsnaveen <getsnaveen@gmail.com>
2025-02-27 09:58:46 +0530

    updated import issues

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.11.11
+COPY . /ominiflow/
+RUN pip install --upgrade pip 
+RUN pip install -r /ominiflow/requirements.txt
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+ENTRYPOINT ["streamlit", "run", "/ominiflow/main.py", "--server.port=8501", "--server.address=0.0.0.0"]
+
--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,6 +1,5 @@
 from preprocessing.transformation import CustomTransfromation
 from preprocessing.imputation import CustomImputer
-from preprocessing.transformation import CustomScaling
 from preprocessing.featureextraction import *
 from preprocessing.featureselection import *
 from preprocessing.filtration import DataFiltration
